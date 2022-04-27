8b9cdd534036fa7ec3d00285563ce4971dd147b3
Tomiwa <72953345+oluwatomiwatemidayo@users.noreply.github.com>
2022-04-27 22:09:21 +0100

    footer file added to repo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,8 +5,35 @@ import './footer.css'
 const Footer = () => {
   return (
    <footer>
-  
+      <div className="links-and-contact">
 
+        <div className="links">
+          <h3>Links</h3>
+          <ul className="links-list">
+            <li><a href="https://facebook.com/">Facebook</a></li>
+            <li><a href="https://twitter.com/">Twitter</a></li>
+            <li><a href="https://instagram.com/">Intagram</a></li>
+          </ul>
+        </div>
+
+
+        <div>
+          <form action="#">
+            <label for="email">
+              <h4>Email</h4>
+              <input type="email" id="email" placeholder="Enter your email"/>
+            </label>
+            <label for="message">
+              <h4>Message</h4>
+              <textarea id="message">Your Message</textarea>
+            </label>
+            <div className="submit-button-wrapper">
+              <input className="submit" type="submit" valid="Send Message" id="submit-button" />
+            </div>
+          </form>
+        </div>
+
+</div>
 
    </footer>
   )
